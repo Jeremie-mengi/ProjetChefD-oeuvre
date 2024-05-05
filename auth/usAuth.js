9bec1acc25dc65e0c40a6e6d3b18e30af19734b9
Jeremie-mengi <jeremiemengi248@gmail.com>
2024-05-05 23:37:56 +0100

    mybackFinish

--- auth/usAuth.js
+++ auth/usAuth.js
@@ -1,32 +1,33 @@
-const bcrypt = require ("bcrypt");
+const bcrypt = require("bcrypt");
 const passport = require("passport");
 const prisma = require("../db/prisma");
 const LocalStrategy = require("passport-local").Strategy
 
-const configPassport = new LocalStrategy({usernameField:"email", passwordField:"password"}, 
-async(email,passport,done)=>{
-console.log(configPassport);
-    const user = await prisma.user.findFirst({where:{email}})
-    if(!user){
-        return done(null,false,{message:"User introuvable"})
-    }
-    bcrypt.compare(passport,user.password, (err,IsMatch)=>{
-    if(!user){
-        return done (err)
-    }
-    if (!IsMatch){
-        return done (null,false,{message: "Mot de passe incorect"});
-    }
+const configPassport = new LocalStrategy({ usernameField: "email", passwordField: "password" },
+    async (email, passport, done) => {
+        const user = await prisma.user.findFirst({ where: { email } })
+        if (!user) {
+            return done(null, false, { message: "User introuvable" })
+        }
+        bcrypt.compare(passport, user.password, (err, IsMatch) => {
+            if (!user) {
+                return done(err)
+            }
+            if (!IsMatch) {
+                return done(null, false, { message: "Mot de passe incorect" });
+            } else {
+                return done(null, true, 'user find')
+            }
+        })
     })
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+passport.deserializeUser((user, done) => {
+    done(null, user)
 })
 
-passport.serializeUser((user,done) => {
-    done(null, user.id)
-  })
-  passport.deserializeUser((user,done) => {
-    done(null, user)
-  })
-    
-  module.exports = configPassport ;
+module.exports = configPassport;
 
 
--- midellware/authMidlUser.js
+++ midellware/authMidlUser.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
-const { user } = require("../db/prisma");
+const prisma = require("../db/prisma");
 
 async function authMidlUser(req, res, next) {
     passport.authenticate("local", async (err, user, info) => {
@@ -9,13 +9,13 @@ async function authMidlUser(req, res, next) {
                 return next(err);
             }
             if (!user) {
-                return res.status(401).json({ message: 'Identifiant incorrect' });
+                return res.status(401).json({ message: 'User non trouvé' });
             }
-            
+
             // Vérification des identifiants de l'utilisateur dans la base de données
-            const foundUser = await user.findUnique({ where: { email: req.body.email } });
+            const foundUser = await prisma.user.findUnique({ where: { email: req.body.email } });
             if (!foundUser) {
-                return res.status(401).json({ message: 'Identifiant incorrect' });
+                return res.status(401).json({ message: 'L\'utilisateur n\'existe  pas avec cette adresse : ' + req.body.email });
             }
 
             // Si l'utilisateur est trouvé dans la base de données, nous le connectons
@@ -26,9 +26,9 @@ async function authMidlUser(req, res, next) {
 
                 // Création du token JWT
                 const token = jwt.sign({ id: foundUser.id, role: foundUser.role }, process.env.JWT_SECRET, { expiresIn: '3m' });
-                
+
                 // Envoi du token JWT en réponse
-                return res.status(200).json({ token });
+                return res.status(200).json(token);
             });
         } catch (error) {
             return next(error);
