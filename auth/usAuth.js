c1a77afa5d6e0a50f4af47e61407b25970254f5a
Jeremie-mengi <jeremiemengi248@gmail.com>
2024-05-10 01:36:05 +0100

    bcryptJs

--- auth/usAuth.js
+++ auth/usAuth.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const passport = require("passport");
 const prisma = require("../db/prisma");
 const LocalStrategy = require("passport-local").Strategy
