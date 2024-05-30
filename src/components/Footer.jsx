edbf491913f4a1226dbce53252f3640e62ba47df
Jeremie-mengi <jeremiemengi248@gmail.com>
2024-05-30 14:03:46 +0100

    responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,53 +1,56 @@
 function Footer() {
     return(
-        <section className="bg-[#000000] rounded-none m-[auto] flex flex-col justify-between h-screen">
-        <div className="flex flex flex-col items-center  text-white my-[120px] gap-[20px]">
-            <p className="text-lg font-bold ">BENEFITS</p>
-            <p className="text-4xl font-bold font-[inter]">Unlocking the power of third </p>
-            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit At nobis nihil.</p>
-            <p>At nobis nihil adipisci veritatis natus labore ab, </p>
-            <button className="mr-4 bg-[#EEEAEA] rounded-md  w-[200px] h-[50px] text-black text-base font-[inter] ">Contactez-Nous</button>
+        <section className="bg-[#000000] rounded-none m-auto flex flex-col justify-between min-h-screen">
+        <div className="flex flex-col items-center text-white my-[60px] sm:my-[120px] gap-[20px] px-4 sm:px-0">
+            <p className="text-lg font-bold">BENEFITS</p>
+            <p className="text-2xl sm:text-4xl font-bold font-[inter] text-center">Unlocking the power of third</p>
+            <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit At nobis nihil.</p>
+            <p className="text-center">At nobis nihil adipisci veritatis natus labore ab,</p>
+            <button className="mr-0 sm:mr-4 bg-[#EEEAEA] rounded-md w-[200px] h-[50px] text-black text-base font-[inter] mt-4">Contactez-Nous</button>
         </div>
     
-     
-        <div className="text-white flex flex-col justify-between w-[1400px] gap-[40px] "> 
-        <hr className="border-white-400 my-[20px] w-[1320px] opacity-20" />
-        <div className="text-white flex flex-rows justify-between w-[1400px] "> 
-            <div className="flex flex-col gap-[15px]">
-                <h1 className="font-bold">Voir</h1>
-                <p>Plus</p>
-                <p>Moins</p>
-                <p>Extra</p>
-            </div>
-            <div className="flex flex-col gap-[15px]">
-                <h1 className="font-bold">Voir</h1>
-                <p>Plus</p>
-                <p>Moins</p>
-                <p>Extra</p>
-            </div>
-            <div className="flex flex-col gap-[15px]">
-                <h1 className="font-bold">Voir</h1>
-                <p>Plus</p>
-                <p>Moins</p>
-                <p>Extra</p>
-            </div>
-            <div className="flex flex-col gap-[15px]">
-                <h1 className="font-bold">Voir</h1>
-                <p>Plus</p>
-                <p>Moins</p>
-                <p>Extra</p>
-            </div>
-    
-            <div className="flex flex-col gap-[15px]">
-                <h1 className="font-bold">Voir</h1>
-                <p>Plus</p>
-                <p>Moins</p>
-                <p>Extra</p>
-            </div>
+        <div className="text-white flex flex-col w-full px-4 sm:px-0 gap-[20px] sm:gap-[40px]">
+            <hr className="border-white-400 my-[20px] opacity-20" />
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 sm:gap-[15px]">
+                <div className="flex flex-col ml-[50px]">
+                    <h1 className="font-bold">Voir</h1>
+                    <p>Plus</p>
+                    <p>Moins</p>
+                    <p>Extra</p>
+                </div>
+                <div className="flex flex-col ml-[50px]">
+                    <h1 className="font-bold">Voir</h1>
+                    <p>Plus</p>
+                    <p>Moins</p>
+                    <p>Extra</p>
+                </div>
+                <div className="flex flex-col ml-[50px]">
+                    <h1 className="font-bold">Voir</h1>
+                    <p>Plus</p>
+                    <p>Moins</p>
+                    <p>Extra</p>
+                </div>
+                <div className="flex flex-col ml-[50px]">
+                    <h1 className="font-bold">Voir</h1>
+                    <p>Plus</p>
+                    <p>Moins</p>
+                    <p>Extra</p>
+                </div>
+                <div className="flex flex-col ml-[50px]">
+                    <h1 className="font-bold">Voir</h1>
+                    <p>Plus</p>
+                    <p>Moins</p>
+                    <p>Extra</p>
+                </div>
+                <div className="flex flex-col ml-[50px]">
+                    <h1 className="font-bold">Voir</h1>
+                    <p>Plus</p>
+                    <p>Moins</p>
+                    <p>Extra</p>
+                </div>
             </div>
         </div>
     </section>
-
     )
     
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -26,62 +26,65 @@ function Main() {
                 </div>
             </section>
     
-            <section class="flex flex-col items-center md:mt-[-430px] gap-[2px] text-center px-4">
-                <p class="text-lg font-bold">BENEFITS</p>
-                <p class="text-2xl md:text-4xl font-bold font-[inter]">Unlocking the power of third</p>
-                <p class="text-2xl md:text-4xl font-bold font-[inter] mb-[26px]">party payments</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit At nobis nihil.</p>
-                <p>At nobis nihil adipisci veritatis natus labore ab,</p>
+            <section class="flex flex-col items-center md:mt-[-430px] gap-2 text-center px-4">
+           <p class="text-lg md:text-xl font-bold">AVANTAGES</p>
+           <p class="text-2xl md:text-4xl font-bold font-[inter]">Déverrouiller la puissance des</p>
+           <p class="text-2xl md:text-4xl font-bold font-[inter] mb-6 md:mb-10">paiements tiers</p>
+           <p class="text-base md:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. At nobis nihil.</p>
+           <p class="text-base md:text-lg">At nobis nihil adipisci veritatis natus labore ab.</p>
             </section>
+
+            <section class="flex flex-col md:flex-row items-center mt-[40px] md:mt-[-370px] gap-5 m-auto px-4">
+    <div class="bg-[#65F6B9] w-full md:w-[600px] h-auto md:h-[800px] flex flex-col rounded-[15px] p-4">
+        <div class="py-4 pb-20 px-10 font-[inter]">
+            <p class="text-lg font-bold">Forterest paiement</p>
+            <p class="text-lg">Bon et exceptionnel pour vos écoles, félicitations à vous et exceptionnel pour vos écoles, félicitations</p>
+        </div>
+        <div class="bg-[#FFFFFF] mx-7 rounded-[10px] p-4">
+            <h1 class="mx-7 mb-6 font-bold">Détails de paiement</h1>
+            <hr />
+            <form action="" class="flex flex-col space-y-4 mx-7 border border-gray-300 p-2 my-4 rounded-[19px]">
+                <label for="email1">Adresse Email</label>
+                <input id="email1" type="text" class="border border-gray-300 p-2 rounded-[10px]" />
+                <label for="email2">Adresse Email</label>
+                <input id="email2" type="text" class="border border-gray-300 p-2 rounded-[10px]" />
+            </form>
+            <h1 class="mx-7">Détails de paiement</h1>
+            <form action="" class="flex flex-col space-y-4 mx-7 border border-gray-300 p-2 my-2 mb-6 rounded-[19px]">
+                <label for="email3">Adresse Email</label>
+                <input id="email3" type="text" class="border border-gray-300 p-2 rounded-[10px]" />
+            </form>
+        </div>
+    </div>
+
+    <div class="flex flex-col justify-between gap-5 w-full md:w-auto">
+        <div class="bg-[#6557FF] w-full md:w-[600px] h-auto md:h-[390px] rounded-[15px] p-4 flex flex-col justify-end">
+            <p class="text-lg font-bold text-white">Forterest paiement</p>
+            <p class="text-lg text-white">Bon et exceptionnel pour vos écoles, félicitations à vous et exceptionnel pour vos écoles, félicitations</p>
+        </div>
+        <div class="bg-[#000000] w-full md:w-[600px] h-auto md:h-[390px] rounded-[15px] p-4 flex flex-col justify-end">
+            <p class="text-lg font-bold text-white">Forterest paiement</p>
+            <p class="text-lg text-white">Bon et exceptionnel pour vos écoles, félicitations à vous et exceptionnel pour vos écoles, félicitations</p>
+        </div>
+    </div>
+</section>
+
     
-            <section class="flex flex-col md:flex-row items-center mt-[40px] md:mt-[-370px] gap-[20px] m-auto px-4">
-                <div class="bg-[#65F6B9] w-full md:w-[600px] h-auto md:h-[800px] flex flex-col rounded-[15px] p-4">
-                    <div class="py-4 pb-[90px] px-[40px] font-[inter]">
-                        <p class="text-lg font-bold">Forterest paiement</p>
-                        <p class="text-lg">Bon et exceptionnel pour vos écoles, félicitations à vous et exceptionnel pour vos écoles, félicitations</p>
-                    </div>
-                    <div class="bg-[#FFFFFF] mx-[30px] rounded-[10px] p-4">
-                        <h1 class="mx-[30px] mb-[26px] font-bold">Détails de paiement</h1>
-                        <hr />
-                        <form action="" class="flex flex-col space-y-4 mx-[30px] border border-gray-300 p-2 my-[18px] rounded-[19px]">
-                            <label for="email1">Adresse Email</label>
-                            <input id="email1" type="text" class="border border-gray-300 p-2 rounded-[10px]" />
-                            <label for="email2">Adresse Email</label>
-                            <input id="email2" type="text" class="border border-gray-300 p-2 rounded-[10px]" />
-                        </form>
-                        <h1 class="mx-[30px]">Détails de paiement</h1>
-                        <form action="" class="flex flex-col space-y-4 mx-[30px] border border-gray-300 p-2 my-2 mb-[26px] rounded-[19px]">
-                            <label for="email3">Adresse Email</label>
-                            <input id="email3" type="text" class="border border-gray-300 p-2 rounded-[10px]" />
-                        </form>
-                    </div>
-                </div>
-    
-                <div class="flex flex-col justify-between gap-[20px] w-full md:w-auto">
-                    <div class="bg-[#6557FF] w-full md:w-[600px] h-auto md:h-[390px] rounded-[15px] p-4 flex flex-col justify-end">
-                        <p class="text-lg font-bold text-white">Forterest paiement</p>
-                        <p class="text-lg text-white">Bon et exceptionnel pour vos écoles, félicitations à vous et exceptionnel pour vos écoles, félicitations</p>
-                    </div>
-                    <div class="bg-[#000000] w-full md:w-[600px] h-auto md:h-[390px] rounded-[15px] p-4 flex flex-col justify-end">
-                        <p class="text-lg font-bold text-white">Forterest paiement</p>
-                        <p class="text-lg text-white">Bon et exceptionnel pour vos écoles, félicitations à vous et exceptionnel pour vos écoles, félicitations</p>
-                    </div>
-                </div>
-            </section>
-    
-            <section class="bg-[#F6F6F6] rounded-none flex flex-col items-center py-4">
-                <div class="text-left w-full md:w-auto md:mr-[800px] mt-4 md:mt-[-130px] mb-[400px] flex flex-col gap-[10px] px-4">
-                    <p class="text-lg font-bold">BENEFITS</p>
-                    <p class="text-2xl md:text-4xl font-bold font-[inter]">Unlocking the power of third</p>
-                    <p class="text-base font-[inter] font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit At nobis nihil.</p>
-                </div>
-    
-                <div class="flex flex-row w-full md:w-[500px] mt-4 md:mt-[60px] md:mr-[900px] gap-[10px] px-4">
-                    <img class="mx-2 w-1/3" src="path/to/image1.jpg" alt="Image 1" />
-                    <img class="mx-2 w-1/3" src="path/to/image2.jpg" alt="Image 2" />
-                    <img class="mx-2 w-1/3" src="path/to/image3.jpg" alt="Image 3" />
-                </div>
-            </section>
+<section class="bg-[#F6F6F6] rounded-[10px] flex flex-col items-center py-4">
+    <div class="text-left w-full md:w-auto md:mr-[800px] mt-4 md:mt-[-130px] mb-[400px] flex flex-col gap-2 px-4">
+        <p class="text-lg font-bold">BENEFITS</p>
+        <p class="text-2xl md:text-4xl font-bold font-[inter]">Unlocking the power of third</p>
+        <p class="text-base font-[inter] font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. At nobis nihil.</p>
+    </div>
+
+    <div class="flex flex-row justify-center w-full md:w-[500px] mt-4 md:mt-[60px] gap-2 px-4">
+    <img class="w-1/3 md:w-auto" src={image1} alt="Image 1" />
+    <img class="w-1/3 md:w-auto" src={image2} alt="Image 2" />
+    <img class="w-1/3 md:w-auto" src={image3} alt="Image 3" />
+</div>
+
+</section>
+
         </main>
     </div>
     
